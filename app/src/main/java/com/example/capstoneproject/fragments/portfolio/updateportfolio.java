2a98d475c53ae90c457f5d76679e4c287eb77cb7
DogEnjoyer <86926345+DogEnjoyer@users.noreply.github.com>
2021-12-06 23:42:15 -0500

    added buy
    adding sell soon tm

--- app/src/main/java/com/example/capstoneproject/fragments/portfolio/updateportfolio.java
+++ app/src/main/java/com/example/capstoneproject/fragments/portfolio/updateportfolio.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,13 +14,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.capstoneproject.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class updateportfolio extends AppCompatActivity {
     EditText title_input, pages_input;
     Button update_button, delete_button;
     String id, title, author, pages;
-
+    int saveintialamount;
+    databaseforbalance balanceDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,7 @@ public class updateportfolio extends AppCompatActivity {
         pages_input = findViewById(R.id.popupaddstockcrypto_amount2);
         update_button = findViewById(R.id.popupaddstockcrypto_savebutton2);
         delete_button = findViewById(R.id.popupaddstockcrypto_deletebutton);
-
+        balanceDB = new databaseforbalance(updateportfolio.this);
         getAndSetIntentData();
 
         //Set actionbar title after getAndSetIntentData method
@@ -42,12 +52,74 @@ public class updateportfolio extends AppCompatActivity {
             public void onClick(View view) {
                 //And only then we call this
                 myportfoliodatabase myDB = new myportfoliodatabase(updateportfolio.this);
-                title = title_input.getText().toString().trim();
+
                 pages = pages_input.getText().toString().trim();
+                //saveintialamount = Integer.parseInt(pages) + saveintialamount;
+                //pages = String.valueOf(saveintialamount);
+                AsyncHttpClient client = new AsyncHttpClient();
+
+                /*String testapi = "https://cloud.iexapis.com/stable/stock/market/batch?symbols=" + popup_stockname.getText().toString().trim() +"&types=quote&range=1m&last=5&token=sk_312389e990ff49af9d13a20cc770ec95";
+                 */
+                String testapi = "https://financialmodelingprep.com/api/v3/profile/" + title + "?apikey=d610507a84e6b54992411a018867a0b7";
+                client.get(testapi, new JsonHttpResponseHandler() {
+
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        JSONArray jsonObject = json.jsonArray;
+                        try {
+                            JSONObject p = jsonObject.getJSONObject(0);
+                            /*
+                            JSONObject p = results.getJSONObject(toUpperCase(popup_stockname.getText().toString().trim()));
+
+                            p = p.getJSONObject("quote");
+                            */
+                            try {
+                                int x = Integer.parseInt(pages);
+                                System.out.println((int)Double.parseDouble(p.getString("price")));
+                                x = x * (int)Double.parseDouble(p.getString("price"));
+                                x = returnbalance() - x;
+                                if(x<0){
+                                    Toast.makeText(updateportfolio.this, "Error!, not enough money", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    balanceDB.updateData(String.valueOf(returnbalanceid()),String.valueOf(x));
+                                    saveintialamount = Integer.parseInt(pages) + saveintialamount;
+                                    pages = String.valueOf(saveintialamount);
+                                    myDB.updateData(id, title, author, pages);
+                                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                                    finish();
+                                }
+
+                            } catch (NumberFormatException e) {
+                                Toast.makeText(updateportfolio.this, "Error!, not an integer on stock amount", Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+                        } catch (JSONException e) {
+
+                            e.printStackTrace();
+                            Toast.makeText(updateportfolio.this, "Error!, please check stock name", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        System.out.println("Failed");
+                        Toast.makeText(updateportfolio.this, "Error!, please check stock name", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                /*
                 myDB.updateData(id, title, author, pages);
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
                 finish();
+
+                 */
 
 
             }
@@ -65,7 +137,32 @@ public class updateportfolio extends AppCompatActivity {
 
 
 
-
+    int returnbalance(){
+        Cursor cursor2 = balanceDB.readAllData();
+        int bal = 0;
+        if(cursor2.getCount()==0){
+            return 0;
+        }
+        else {
+            while (cursor2.moveToNext()) {
+                bal = Integer.parseInt(cursor2.getString(1));
+            }
+        }
+        return bal;
+    }
+    int returnbalanceid(){
+        Cursor cursor2 = balanceDB.readAllData();
+        int bal = 0;
+        if(cursor2.getCount()==0){
+            return 0;
+        }
+        else {
+            while (cursor2.moveToNext()) {
+                bal = Integer.parseInt(cursor2.getString(0));
+            }
+        }
+        return bal;
+    }
 
     void getAndSetIntentData() {
         if (getIntent().hasExtra("id") && getIntent().hasExtra("title") &&
@@ -75,7 +172,7 @@ public class updateportfolio extends AppCompatActivity {
             title = getIntent().getStringExtra("title");
             author = getIntent().getStringExtra("author");
             pages = getIntent().getStringExtra("pages");
-
+            saveintialamount = Integer.parseInt(getIntent().getStringExtra("pages"));
             //Setting Intent Data
             title_input.setText(title);
 
